cc87bcaaf1ffaf5681c6ee3b768d043a67f93e5f
ислам <@KOMPUTER>
2023-11-02 13:46:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        bool check = true;
+        for (int i = 0; i < n; i++) ;
+        {
+            a[n] = int.Parse(Console.ReadLine());
+            if (!(a[n] % 5 == 0 && a[n] % 2 != 0)) ;
+            {
+                check = false;
+                return;
+            }
+
+            else
+
+            {
+                check = true;
+            }
+        }
+        Console.WriteLine(check);
+    }
+}
